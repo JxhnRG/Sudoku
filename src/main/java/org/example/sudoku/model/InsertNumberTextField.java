94110276d96727884e2588feceb0fa7bcd697534
SergioART2023 <sergio.rodriguez.trujillo@correounivalle.edu.co>
2024-04-24 22:13:36 -0500

    Elimine clase sin uso

--- src/main/java/org/example/sudoku/model/InsertNumberTextField.java
+++
@@ -1,9 +0,0 @@
-package org.example.sudoku.model;
-
-import javafx.scene.control.TextField;
-import org.example.sudoku.controller.GameController;
-
-public class InsertNumberTextField {
-    private int indice;
-    private int numero;
-    }
